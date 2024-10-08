2de2e6c3adfffa0f04ec738e78a04b2515f2c8f2
Oystein_Falkeid <oystein.fal@gmail.com>
2024-10-08 08:40:07 +0200

    <oppsett av filer og overskrifter>

---
+++ innlevering_5.py
@@ -0,0 +1,35 @@
+# innlevering 5
+
+# Group members:
+
+# Oystein Falkeid
+# e-mail: oystein.falkeid@nmbu.no
+
+# Eirik Mentyjaervi
+# e-mail: eirik.mentyjarvi@nmbu.no
+
+import sys
+from pathlib import Path
+
+#printer til fil i stedenfor terminal
+file = Path('.') / Path('terminal.txt')
+sys.stdout = open(file, 'w')
+
+print('innlevering 5')
+print()
+
+
+
+#-------------------------------------------------------------------------------------------------------------------
+print('Group members:')
+print()
+# Group members
+
+print('Oystein Falkeid')
+print('e-mail: oystein.falkeid@nmbu.no')
+print()
+print('Eirik Mentyjaervi')
+print('e-mail: eirik.mentyjarvi@nmbu.no')   
+print()                  
+
+#-------------------------------------------------------------------------------------------------------------------
