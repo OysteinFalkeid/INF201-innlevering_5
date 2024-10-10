1555a0ebe812619e415aebe672758b7a3696cba6
Oystein_Falkeid <oystein.fal@gmail.com>
2024-10-10 08:54:18 +0200

    <klar for innlevering>

--- innlevering_5.py
+++ innlevering_5.py
@@ -61,33 +61,6 @@ print('------------------------------------------------------------------------'
 #-------------------------------------------------------------------------------------------------------------------
 
 # Task 0: Warmup exercise (0 points)
-# In this exercise, you will implement two functions that perform vector addition. 
-# A vector can be represented as a list of numbers in Python (e.g., [1, 2, 3]).
-
-# 1 Define a function called inplace_add_vectors(vec1, vec2):
-
-# 1 This function should add the corresponding elements of vec2 to vec1.
-# After calling this function, vec1 should be modified to hold the sum of the two vectors.
-
-# Example:
-# v1 = [1, 2, 3]
-# v2 = [4, 5, 6]
-# inplace_add_vectors(v1, v2)
-# print(v1) # Output: [5, 7, 9]
-
-# 2 Define a function called add_vectors(vec1, vec2):
-
-# This function should return a new vector where each element is the sum of the corresponding elements from vec1 and vec2.
-# The input vectors (vec1 and vec2) should remain unchanged.
-
-# Example:
-# v1 = [1, 2, 3]
-# v2 = [4, 5, 6]
-# v3 = add_vectors(v1, v2)
-# print(v1) # Output: [1, 2, 3]
-# print(v3) # Output: [5, 7, 9]
-
-# If you want to check your solution or need help, take a look at this video.
 
 #-------------------------------------------------------------------------------------------------------------------
 print('Task 0: Warmup exercise (0 points)')
@@ -127,31 +100,8 @@ print()
 
 print('------------------------------------------------------------------------')
 #-------------------------------------------------------------------------------------------------------------------
+
 # Task 1: Matrix operations (2 points)
-
-# * Create a large matrix:
-
-# Make a 2D NumPy array of size 5000X5000, filled with random integers between 0 and 100.
-
-# * Write four functions to work with the matrix:
-
-#   1 Mean function: Write a function that calculates the average (mean) of all the numbers in the matrix. 
-#       (Hint: The mean is the sum of all array entries divided by the total number of entries.)
-
-#   2 Variance function: Write a function to calculate the variance of the matrix. 
-#       (Hint: The variance measures how much the entries in the matrix differ from the mean. 
-#       It's calculated as the average of the squared differences from the mean, i.e., where 
-#       and are the array entries (or matrix entries).)
-
-#   3 Sum function: Write a function that returns the total sum of all numbers in the matrix.
-
-#   4 Multiply function: Write a function that returns a new matrix where each number in the input matrix is multiplied by a given number.
-# 
-# * Important Notes:
-
-# The input matrix should not be changed.
-# Be efficient in your implementation to avoid slowing down the program (for example, avoid unnecessary memory use or operations that slow down access to the matrix. Use concepts discussed in the lecture.)
-# Write the code for these functions yourself—don’t use built-in functions from libraries for the calculations.
 
 #-------------------------------------------------------------------------------------------------------------------
 print('Task 1: Matrix operations (2 points)')
@@ -199,30 +149,6 @@ print('------------------------------------------------------------------------'
 #-------------------------------------------------------------------------------------------------------------------
 
 # Task 2: Stencil matrix (3 points)
-# Create a stencil matrix of the form
-
-
-# with 50 by 50 entries. That is, create a matrix with diagonal entries of -2 and off-diagonal entries 
-# (the entries left and right of the diagonal) of 1. The first row is 
-# , i.e., the left off-diagonal entry is missing. The last row is 
-# , i.e., the right off-diagonal entry is missing.
-
-# We now want to compute the dominant eigenvalue of this matrix. 
-# An eigenvalue tells you how a matrix will change certain vectors through multiplication. 
-# If you do not know what an eigenvalue is, that is no problem and this knowledge is not required 
-# to solve this task. If you wish to get a better understanding, you can watch this videoLinks 
-# to an external site. and the videos in this series, but this is really not required. 
-# To compute the dominant eigenvalue, take a random vector 
-#  with 50 entries and multiply it by 
-# , leading to 
-# . Set 
-#  and repeat the process. That is, with our new 
-# , compute 
-# , set 
-#  and proceed. You can use numpy functionality to compute multiplications and norms.  
-# After 100 iterations, print out the value of 
-#  which approximates the dominant eigenvalue. Check the result by computing Lambda, 
-# V = np.linalg.eig(A) and inspecting the largest absolute value in Lambda.
 
 #-------------------------------------------------------------------------------------------------------------------
 print('Task 2: Stencil matrix (3 points)')
@@ -255,12 +181,6 @@ print('------------------------------------------------------------------------'
 #-------------------------------------------------------------------------------------------------------------------
 
 # Task 3: Challenge exercise (0 points)
-# Try to rewrite task 2 so that you never have to store the matrix 
-# . For this, write your own function multiply_efficient(v), which computes the matrix-vector product 
-# without requiring the matrix. To write this function, think about the operations that will be performed when computing 
-#  and check if some of these operations can be left out for our choice of 
-# . Hint: A lot of entries in 
-#  are zero!
 
 #-------------------------------------------------------------------------------------------------------------------
 print('Task 3: Challenge exercise (0 points)')
